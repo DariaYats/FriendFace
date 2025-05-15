c07238f1a56bd4371a3019e43345ae8c8f383b38
Daria Yats <dariayatsyniuk@gmail.com>
2025-05-15 13:58:05 +0200

    function that fetch and decode data

--- FriendFace/UserViewModel.swift
+++ FriendFace/UserViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import SwiftUI
+
+class UserViewModel: ObservableObject {
+    @Published var users: [User] = []
+
+    func fetchUsers() async {
+        guard users.isEmpty else { return }
+
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid URL")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decodedUsers = try JSONDecoder().decode([User].self, from: data)
+            self.users = decodedUsers
+        } catch {
+            print("Failed to fetch or decode data: \(error.localizedDescription)")
+        }
+    }
+}
